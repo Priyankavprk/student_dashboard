b272e4c74234dee3ff2e8453e73041a6a34935dd
priyanka <priyankap@rigbot.com>
2019-07-20 14:46:27 +0530

    Store filtered results

--- src/reducers/students.js
+++ src/reducers/students.js
@@ -1,15 +1,29 @@
 const initialState = {
-  data: []
+  data: [],
+  filteredData: [],
+  searchItem: '',
 }
 
 export default function list(state = initialState, action) {
   switch (action.type) {
-    case 'GET_DATA_SUCCESS' : {
+    case 'GET_DATA_SUCCESS' :
       return {
         ...state,
-        data: action.data
+        data: action.data,
+        filteredData: action.data
       }
-    }
+    case 'SEARCH_RESULT' :
+      return {
+        ...state,
+        filteredData: action.data
+      }
+
+    case 'SET_SEARCH_TEXT' :
+      return {
+        ...state,
+        searchItem: action.text
+      }
+
     default:
       return state;
   }
