ec3dbd92be8683e95f3ae7b6f6be158013a629c4
priyanka <priyankap@rigbot.com>
2019-07-20 11:54:29 +0530

    On component mount fetch the student details

--- src/views/home.js
+++ src/views/home.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 
 import Header from '../components/Header';
 import Cards from '../components/Cards';
+import { getStudentsList } from '../utils';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentList: []
+    }
+  }
+
+  componentDidMount() {
+    getStudentsList();
+  }
+
   render() {
     return (
         <div>
