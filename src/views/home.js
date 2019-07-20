b2ae7647e1e2390bae5afc4e015ecba118a83d2b
priyanka <priyankap@rigbot.com>
2019-07-20 12:27:57 +0530

    Get data from store on mount of component

--- src/views/home.js
+++ src/views/home.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Header from '../components/Header';
 import Cards from '../components/Cards';
-import { getStudentsList } from '../utils';
+import { getStudentsList } from '../actions';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      studentList: []
+
     }
   }
 
   componentDidMount() {
-    getStudentsList();
+     this.props.getStudentsList();
   }
 
   render() {
     return (
         <div>
-          <Header/>
+        <Header/>
+        {this.props.studentsData.length > 0 &&
           <Cards/>
+        }
+        {this.props.studentsData.length === 0 &&
+           <div className="loading"> Loading ... </div>
+        }
         </div>
     );
   }
 }
 
-export default Home;
+Home.propTypes = {
+  getStudentsList: PropTypes.func
+}
+
+const mapStateToProps = state => ({
+   studentsData: state.students.data,
+})
+
+const mapDispatchToProps = dispatch => ({
+  getStudentsList: () => dispatch(getStudentsList()),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
