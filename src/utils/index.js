b1939a5536ce5d1133d051f7ac709c4698d431ad
priyanka <priyankap@rigbot.com>
2019-07-20 12:28:16 +0530

    Remove the utils folder

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-export function getStudentsList() {
-  fetch('https://api.myjson.com/bins/1dlper')
-  .then((response) => {
-    return response
-  })
-  .then((response) => response.json())
-  .then((items) => console.log('/////',items))
-}
