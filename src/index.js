615904d5065087ee2d97be8200322e8e6f98bc9b
priyanka <priyankap@rigbot.com>
2019-07-20 12:27:00 +0530

    Configure the app store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
+import rootReducer from './reducers';
+import Home from './views/home';
+import About from './views/about';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const store = compose(applyMiddleware(thunk))(createStore)(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+   <Router>
+    <Route path="/" exact component={Home}/>
+    <Route path="/about" component={About}/>
+   </Router>
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
