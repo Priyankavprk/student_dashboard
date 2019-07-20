0026970d17b240d398022ce54fc2fd1594825f1f
priyanka <priyankap@rigbot.com>
2019-07-20 14:46:04 +0530

    Filter based on search value entered

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
+import { getFilteredData } from '../../actions';
 import './styles.css';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: this.props.searchItem
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark mb-3">
          <a className="navbar-brand" href="#">
            <h1>{this.props.title}</h1>
          </a>
+         <Form inline>
+          <FormControl type="text" placeholder="Search" className=" mr-sm-2" onChange={e => this.props.getFilteredData({ searchItem: e.target.value})}/>
+          <Button type="submit" onClick={() => this.props.getFilteredData(this.state.searchItem)}>Submit</Button>
+         </Form>
        </nav>
     );
   }
@@ -20,4 +34,16 @@ Header.propTypes = {
 }
 
 
-export default Header;
+const mapStateToProps = state => ({
+   studentsData: state.students.data,
+   searchItem: state.students.searchItem
+})
+
+const mapDispatchToProps = dispatch => ({
+  getFilteredData: (searchText) => dispatch(getFilteredData(searchText)),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header)
