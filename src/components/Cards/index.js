b3c7f0e74f8b16233fe1c10121b11ac069151157
priyanka <priyankap@rigbot.com>
2019-07-20 11:54:56 +0530

    Remove img field from card

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -5,7 +5,6 @@ class List extends Component {
   render() {
     return (
       <Card style={{ width: '18rem' }}>
-       <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
          <Card.Title>Card Title</Card.Title>
          <Card.Text>Text here</Card.Text>
