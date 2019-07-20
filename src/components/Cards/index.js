ab154a1972f34fd6847d825a15d370d6a8f727c5
priyanka <priyankap@rigbot.com>
2019-07-20 13:10:03 +0530

    Show cards as per the data fetched and also add student details on card

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+import './styles.css';
 
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+
+    }
+  }
+
   render() {
+    console.log('//////',this.props.studentsData)
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-         <Card.Title>Card Title</Card.Title>
-         <Card.Text>Text here</Card.Text>
-         <Button variant="primary" onClick={() => this.props.navigate.push('/about')}>Go somewhere</Button>
-        </Card.Body>
-       </Card>
+      <div className="container">
+      {this.props.studentsData.map(function(student, index){
+        return (
+          <Card key={index} style={{ width: '18rem' }}>
+            <Card.Body>
+             <Card.Title>{student.name}</Card.Title>
+             <Card.Subtitle className="mb-2 text-muted">Roll number: {student.rollNo}</Card.Subtitle>
+             <Card.Text>
+               Total Marks: {Object.values(student.marks).reduce((acc, val) => {return acc + val;})}
+             </Card.Text>
+             <Button variant="primary" onClick={() => this.props.navigate.push('/about')}>Show Details</Button>
+            </Card.Body>
+          </Card>
+        )
+      })}
+      </div>
     );
   }
 }
 
-export default List;
+const mapStateToProps = state => ({
+   studentsData: state.students.data,
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(List)
